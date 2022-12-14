0e6512f8ef23b2bd8f7e9789721423469b5290bf
Jose Rios <jrbernabe@gmail.com>
2022-12-14 10:49:40 -0400

    Add examples of matchers

---
+++ demos/src/04-assertions.test.js
@@ -0,0 +1,24 @@
+// matchers
+test('test obj', () => {
+  const data = { name: 'nico' };
+  data.lastname = 'molina';
+  expect(data).toEqual({ name: 'nico', lastname: 'molina' });
+});
+
+test('null', () => {
+  let data = null;
+  expect(data).toBeNull();
+  expect(data).toBeDefined();
+
+  data = undefined;
+  expect(data).toBeUndefined();
+});
+
+test('string', () => {
+  expect('Christoph').toMatch(/stop/);
+});
+
+test('list / arrays', () => {
+  const numbers = [1, 2, 3, 4];
+  expect(numbers).toContain(3);
+});
